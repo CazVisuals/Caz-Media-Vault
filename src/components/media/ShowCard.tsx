8add2eff8685b7a7336c5da7609fa568dd6ed158
CazVisuals <info@cazvisuals.com>
2026-08-18 11:27:58 -0500

    Keep show thumbnails uniform with movie cards

--- src/components/media/ShowCard.tsx
+++ src/components/media/ShowCard.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import type { Movie } from "@/lib/media/types";
+import styles from "./MediaCard.module.css";
 
 export function ShowCard({ show, episodeCount }: { show: Movie; episodeCount: number }) {
-  return <Link className="media-card focusable" href={`/tv/show/${show.id}`} data-focusable="true">
-    <div className="poster-shell">
+  return <Link className={`media-card focusable ${styles.card}`} href={`/tv/show/${show.id}`} data-focusable="true">
+    <div className={`poster-shell ${styles.posterShell}`}>
       {/* eslint-disable-next-line @next/next/no-img-element -- local NAS artwork supports older TV browsers */}
       {show.posterUrl ? <img src={show.posterUrl} alt="" className="poster" /> : <div className="poster-fallback"><span>CH</span></div>}
       <span className="episode-count">{episodeCount} {episodeCount === 1 ? "episode" : "episodes"}</span>
